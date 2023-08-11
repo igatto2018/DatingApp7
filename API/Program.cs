afda45f24dfdacde6393bbc7f9a5470085db046f
Ignazio Gatto <gattoignazio@hotmail.com>
2023-08-11 23:11:04 +0200

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -14,7 +14,7 @@ var app = builder.Build();
 app.UseCors(builder => 
   builder.AllowAnyHeader()
   .AllowAnyMethod()
-  .WithOrigins("https://localhost:4200")
+  .WithOrigins("http://localhost:4200")
 );
 
 app.UseAuthentication();
